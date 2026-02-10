c25d0a1ab2a8d1e5caaf5eeabb6b93cdd8dc4908
jagadeesh877 <jaga543424@gmail.com>
2026-02-10 15:39:34 +0530

    10/09 final commit

--- server/debug_depts.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-async function checkDepts() {
-    const depts = await prisma.department.findMany({ select: { id: true, name: true, code: true } });
-    console.log(JSON.stringify(depts, null, 2));
-}
-
-checkDepts().catch(console.error).finally(() => prisma.$disconnect());
